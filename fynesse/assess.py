458f571aad1ab88c7dd159bf8467eb3f2d508d30
Ana <anamradu2406@gmail.com>
2021-12-13 19:51:37 +0200

    commit

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -29,23 +29,6 @@ TAGS = {"amenity": True,
         "sport": True, 
         "public_transport": True}
 
-def data():
-    """Load the data from access and ensure missing values are correctly encoded as well as indices correct, column names informative, date and times correctly formatted. Return a structured data structure such as a data frame."""
-    df = access.data()
-    raise NotImplementedError
-
-def query(data):
-    """Request user input for some aspect of the data."""
-    raise NotImplementedError
-
-def view(data):
-    """Provide a view of the data that allows the user to verify some aspect of its quality."""
-    raise NotImplementedError
-
-def labelled(data):
-    """Provide a labelled set of data ready for supervised learning."""
-    raise NotImplementedError
-
 def assess_house_prices(region):
     print(f'Assessing dataframe...')
     required_columns = ['price', 'date_of_transfer', 'property_type', 'latitude', 'longitude', 'postcode']
@@ -86,3 +69,21 @@ def view_pois_points(latitude, longitude, tags=TAGS, box_radius=0.005):
     print("Around point with latitude " + str(pois_list.pop()) + " and logitutde " + str(pois_list.pop()) + " on a radius of " +  box_radius*111 + "km there are a number of pois points of type: ")
     for tag in tags:
         print(tag + ": " + str(pois_list.pop()))
+
+        
+
+def data():
+    """Load the data from access and ensure missing values are correctly encoded as well as indices correct, column names informative, date and times correctly formatted. Return a structured data structure such as a data frame."""
+    df = access.data()
+
+
+def query(data):
+    """Request user input for some aspect of the data."""
+    
+
+def view(data):
+    """Provide a view of the data that allows the user to verify some aspect of its quality."""
+    
+
+def labelled(data):
+    """Provide a labelled set of data ready for supervised learning."""
